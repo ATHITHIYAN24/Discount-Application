6b3d7484d72439e91584ef14b9d9900a492978de
LAPTOP-91UHOKAL\athit <athithya.prabath@gmail.com>
2023-05-31 14:57:56 +0530

    Exception changes added

--- src/main/kotlin/com/example/discountlogic/controller/DiscountController.kt
+++ src/main/kotlin/com/example/discountlogic/controller/DiscountController.kt
@@ -5,11 +5,11 @@ import com.example.discountlogic.data.PurchaseDetails
 import com.example.discountlogic.service.DiscountService
 import io.swagger.annotations.Api;
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import java.math.BigDecimal;
 import javax.validation.Valid
 
 @RestController
@@ -18,11 +18,19 @@ import javax.validation.Valid
 
         var logger = LoggerFactory.getLogger(DiscountController::class.java)
 
-        @PostMapping("/discountedBillAmount")
-        fun yourPostMethod(@Valid @RequestBody requestBody: PurchaseDetails): ResponseEntity<BigDecimal> {
-            logger.info("DiscountController :: Request received to calculate final bill amount :: Started")
-            val response = discountService.calculateFinalAmount(requestBody)
-            logger.info("DiscountController :: Request received to calculate final bill amount :: Ended")
-            return ResponseEntity.ok(response)
+    @PostMapping("/discountedBillAmount")
+    fun getNetPayableAmount(@Valid @RequestBody requestBody: PurchaseDetails): ResponseEntity<String> {
+        logger.info("DiscountController :: Request received to calculate final bill amount :: Started")
+        val response: String
+        try {
+            response = discountService.calculateFinalAmount(requestBody).toString()
+        } catch (e: Exception) {
+            logger.error("DiscountController :: Error occurred while calculating final bill amount: {}", e.message)
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(e.message)
         }
+        logger.info("DiscountController :: Request received to calculate final bill amount :: Ended")
+        return ResponseEntity.ok(response)
+    }
+
 }
--- src/main/kotlin/com/example/discountlogic/controller/UserController.kt
+++ src/main/kotlin/com/example/discountlogic/controller/UserController.kt
@@ -1,5 +1,4 @@
-package com.example.discountlogic.controller;
-
+package com.example.discountlogic.controller
 
 import com.example.discountlogic.data.UserData
 import com.example.discountlogic.model.UserDetails
@@ -7,26 +6,35 @@ import com.example.discountlogic.service.UserService
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.text.ParseException
 import javax.validation.Valid
 
 @RestController
+@RequestMapping("/user")
 @Api(value = "UserDetails")
 class UserController(private val userService: UserService) {
 
-    var logger = LoggerFactory.getLogger(UserController::class.java)
+    private val logger = LoggerFactory.getLogger(UserController::class.java)
 
-    @PostMapping("/user")
+    @PostMapping
     @ApiOperation(value = "Add User", response = UserDetails::class)
-    @Throws(ParseException::class)
     fun saveUserDetails(@Valid @RequestBody userData: UserData): ResponseEntity<UserDetails> {
-        logger.info("UserController :: Request received to save the user details  :: Started")
-        val response = userService.saveUserDetails(userData)
-        logger.info("UserController :: Request received to save the user details  :: Ended")
-        return ResponseEntity.ok(response);
+        logger.info("UserController :: Request received to save the user details :: Started")
+        val response: UserDetails
+        try {
+            response = userService.saveUserDetails(userData)
+        } catch (e: Exception) {
+            logger.error("UserController :: Error occurred while saving user details: {}", e.message)
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(null) // Or return a custom error message if desired
+        }
+        logger.info("UserController :: Request received to save the user details :: Ended")
+        return ResponseEntity.ok(response)
     }
+
 }
